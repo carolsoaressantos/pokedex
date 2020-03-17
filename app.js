07ae0ce1f3f0b1a59dc426dd5567a5df653360e7
carolcodes <carolinesoaressantoss@gmail.com>
2020-03-16 23:29:12 -0300

    Corrige link da API

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const getPokemonUrl = id => `https://pokeapi.co/api/v2/${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
 const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
     fetch(getPokemonUrl(index + 1)).then(response => response.json()))
